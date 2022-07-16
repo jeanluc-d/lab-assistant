5bcc080042e6e6567ccf56610d8987554a46ed21
Jean-Luc Desjardins <70818495+JLDCodes@users.noreply.github.com>
2022-07-16 13:18:05 -0400

    responsive button placement

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -43,7 +43,7 @@ function Search() {
           {input !== "" && (
             <button
               type="submit"
-              className="absolute top-0 right-0 mt-5 mr-10 focus:outline-none"
+              className="absolute top-0 mt-5 right-6 sm:right-12 md:right-0 md:mr-2 focus:outline-none"
               onClick={() => handleClear()}
             >
               <svg
@@ -62,7 +62,7 @@ function Search() {
               </svg>
             </button>
           )}
-          <button
+          {/* <button
             onClick={() => getLab(input)}
             aria-label="Search labs"
             className="absolute top-0 right-0 mt-5 mr-5 focus:outline-none"
@@ -79,7 +79,7 @@ function Search() {
             >
               <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
             </svg>
-          </button>
+          </button> */}
         </div>
       </div>
       {searchData?.length > 0 && (
